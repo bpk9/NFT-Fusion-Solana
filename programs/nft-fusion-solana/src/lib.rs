446114b59bdede2ce397b433c0d881306a688d60
Brian Kasper <bkasperr@gmail.com>
2024-07-13 19:27:39 -0400

    Rename authority account to signer

--- programs/nft-fusion-solana/src/lib.rs
+++ programs/nft-fusion-solana/src/lib.rs
@@ -11,7 +11,7 @@ pub mod nft_fusion_solana {
         let cpi_accounts = MintTo {
             mint: ctx.accounts.mint.to_account_info(),
             to: ctx.accounts.token_account.to_account_info(),
-            authority: ctx.accounts.authority.to_account_info(),
+            authority: ctx.accounts.signer.to_account_info(),
         };
         let cpi_program = ctx.accounts.token_program.to_account_info();
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
@@ -25,8 +25,11 @@ pub mod nft_fusion_solana {
 pub struct MintNFT<'info> {
     #[account(mut)]
     pub mint: Account<'info, Mint>,
+
+    pub signer: Signer<'info>,
+    
     #[account(mut)]
     pub token_account: Account<'info, TokenAccount>,
-    pub authority: Signer<'info>,
+    
     pub token_program: Program<'info, Token>,
 }
